1eebabc8f4aaab5e707c42ab15d963e993180b37
Ted M. Young <tedyoung@gmail.com>
2021-11-10 15:10:02 -0800

    LAB #4: Extracted Suit enum, still in progress (all tests pass)

---
+++ src/main/java/com/jitterted/ebp/blackjack/Suit.java
@@ -0,0 +1,35 @@
+package com.jitterted.ebp.blackjack;
+
+import java.util.List;
+
+public enum Suit {
+    SPADES("♠"),
+    DIAMONDS("♦"),
+    HEARTS("♥"),
+    CLUBS("♣");
+
+    static final List<String> SUITS = List.of(SPADES.symbol(),
+                                              DIAMONDS.symbol(),
+                                              HEARTS.symbol(),
+                                              CLUBS.symbol());
+    private final String symbol;
+
+    Suit(String symbol) {
+        this.symbol = symbol;
+    }
+
+    // SCAFFOLDING/BRIDGE
+    @Deprecated
+    public static Suit from(String symbol) {
+        for (Suit suit : values()) {
+            if (suit.symbol().equals(symbol)) {
+                return suit;
+            }
+        }
+        return null;
+    }
+
+    public String symbol() {
+        return symbol;
+    }
+}
