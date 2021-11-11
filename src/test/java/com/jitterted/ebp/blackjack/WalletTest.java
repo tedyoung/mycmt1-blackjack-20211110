0f00ef0d3f2ed8d92c72ca5c547de6fa45fd6dc1
Ted M. Young <tedyoung@gmail.com>
2021-11-11 14:32:23 -0800

    TDD: addMoney now implemented and passes tests

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -24,4 +24,33 @@ public class WalletTest {
                 .isFalse();
     }
 
+    @Test
+    public void newWalletHasZeroBalance() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThat(wallet.balance())
+                .isZero();
+    }
+
+    @Test
+    public void newWalletAdd7ThenBalanceIs7() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(7);
+
+        assertThat(wallet.balance())
+                .isEqualTo(7);
+    }
+
+    @Test
+    public void newWalletAdd10And5ThenBalanceIs15() throws Exception {
+        Wallet wallet = new Wallet();
+
+        wallet.addMoney(10);
+        wallet.addMoney(5);
+
+        assertThat(wallet.balance())
+                .isEqualTo(10 + 5);
+    }
+
 }
