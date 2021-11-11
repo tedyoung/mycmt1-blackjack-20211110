173295628ab27050ff60c46fd5c99da07aed3f51
Ted M. Young <tedyoung@gmail.com>
2021-11-11 15:00:24 -0800

    TDD: addMoney throws exception for Zero as well

--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -62,4 +62,13 @@ public class WalletTest {
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    public void addZeroMoneyThrowsException() throws Exception {
+        Wallet wallet = new Wallet();
+
+        assertThatThrownBy(() -> {
+            wallet.addMoney(0);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
