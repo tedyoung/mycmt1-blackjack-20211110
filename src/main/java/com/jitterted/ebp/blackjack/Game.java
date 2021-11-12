d29a3f8588fddbbf2af929841e937a74a4e43601
Ted M. Young <tedyoung@gmail.com>
2021-11-11 16:16:42 -0800

    Player bet payoff works for Player Wins scenario.

--- src/main/java/com/jitterted/ebp/blackjack/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/Game.java
@@ -13,6 +13,8 @@ public class Game {
 
     private final Hand dealerHand = new Hand();
     private final Hand playerHand = new Hand();
+    private int playerBalance = 0;
+    private int playerBetAmount = 0;
 
     public static void main(String[] args) {
         prepareScreen();
@@ -85,14 +87,19 @@ public class Game {
     private void displayGameOutcome(boolean playerBusted) {
         if (playerBusted) {
             System.out.println("You Busted, so you lose.  💸");
+//            playerLoses();
         } else if (dealerHand.isBusted()) {
             System.out.println("Dealer went BUST, Player wins! Yay for you!! 💵");
+//            playerWins();
         } else if (playerHand.beats(dealerHand)) {
             System.out.println("You beat the Dealer! 💵");
+//            playerWins();
         } else if (playerHand.pushes(dealerHand)) {
             System.out.println("Push: You tie with the Dealer. 💸");
+//            playerPushes();
         } else {
             System.out.println("You lost to the Dealer. 💸");
+//            playerLoses();
         }
     }
 
@@ -178,5 +185,22 @@ public class Game {
         System.out.println("Player has: ");
         playerHand.display();
         System.out.println(" (" + playerHand.value() + ")");
+    }
+
+    public int playerBalance() {
+        return playerBalance;
+    }
+
+    public void playerDeposits(int amount) {
+        playerBalance += amount;
+    }
+
+    public void playerBets(int betAmount) {
+        playerBalance -= betAmount;
+        playerBetAmount = betAmount;
+    }
+
+    public void playerWins() {
+        playerBalance += playerBetAmount * 2;
     }
 }
